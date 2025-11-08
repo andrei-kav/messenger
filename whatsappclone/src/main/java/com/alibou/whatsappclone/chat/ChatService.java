402f8cb0bc8c701f873605e1e479557c0113b23d
andreiKav <andrei.kav9307@gmail.com>
2025-11-08 18:15:48 +0300

    chat entities implemented

---
+++ whatsappclone/src/main/java/com/alibou/whatsappclone/chat/ChatService.java
@@ -0,0 +1,49 @@
+package com.alibou.whatsappclone.chat;
+
+import com.alibou.whatsappclone.user.User;
+import com.alibou.whatsappclone.user.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ChatService {
+
+    private final ChatRepository chatRepository;
+    private final UserRepository userRepository;
+    private final ChatMapper mapper;
+
+    @Transactional(readOnly = true)
+    public List<ChatResponse> getChatsByReceiverId(Authentication currentUser) {
+        final String userId = currentUser.getName();
+        return chatRepository.findChatsBySenderId(userId)
+                .stream()
+                .map(chat -> mapper.toChatResponse(chat, userId))
+                .toList();
+    }
+
+    public String createChat(String senderId, String receiverId) {
+        Optional<Chat> existingChat = chatRepository.findChatsBySenderIdAndReceiverId(senderId, receiverId);
+        if (existingChat.isPresent()) {
+            return existingChat.get().getId();
+        }
+        User sender = userRepository.findByPublicId(senderId)
+                .orElseThrow(() -> new EntityNotFoundException("User with id " + senderId + " not found"));
+        User receiver = userRepository.findByPublicId(senderId)
+                .orElseThrow(() -> new EntityNotFoundException("User with id " + receiverId + " not found"));
+
+        Chat chat = new Chat();
+        chat.setSender(sender);
+        chat.setRecipient(receiver);
+
+        Chat savedChat = chatRepository.save(chat);
+        return savedChat.getId();
+    }
+
+}
