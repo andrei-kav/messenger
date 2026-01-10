1f94d6af27afedfbe1e9a6399b99dd4fcfff8521
andreiKav <andrei.kav9307@gmail.com>
2026-01-10 15:42:51 +0300

    chat-list implemented

--- messenger-ui/src/app/components/chat-list/chat-list.ts
+++ messenger-ui/src/app/components/chat-list/chat-list.ts
@@ -1,14 +1,49 @@
-import {Component, input, InputSignal} from '@angular/core';
+import {Component, inject, input, InputSignal, signal, WritableSignal} from '@angular/core';
 import {ChatResponse} from '../../services/models/chat-response';
+import {DatePipe} from '@angular/common';
+import {UserResponse} from '../../services/models/user-response';
+import {Api} from '../../services/api';
+import {getAllUsers} from '../../services/functions';
 
 @Component({
   selector: 'app-chat-list',
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './chat-list.html',
   styleUrl: './chat-list.scss',
 })
 export class ChatList {
 
+  private api: Api = inject(Api);
+
   chats: InputSignal<ChatResponse[]> = input<ChatResponse[]>([]);
 
+  searchNewContact: WritableSignal<boolean> = signal(false);
+  contacts: WritableSignal<UserResponse[]> = signal<UserResponse[]>([]);
+
+  constructor() {}
+
+  searchContact(): void {
+    this.api.invoke(getAllUsers)
+      .then((users: UserResponse[]) => {
+        this.contacts.set(users);
+        this.searchNewContact.set(true);
+      })
+  }
+
+  onChatClicked(chat: ChatResponse): void {
+
+  }
+
+  wrapMessage(lastMessage: string | undefined): string {
+    if (lastMessage && lastMessage.length <= 20) {
+      return lastMessage;
+    }
+    return lastMessage?.substring(0, 17) + "...";
+  }
+
+  selectContact(): void {
+
+  }
 }
